9c8d7e4137ccf4627b94d519937b748e62debe6d
Kendrick Gardner <kbgar24@gmail.com>
2017-10-04 18:54:34 -0500

    Splitting Redux Store into more manageable components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import { getVisibleExpenses } from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
+
+const store = configureStore();
+
+// addExpense
+store.dispatch(addExpense({
+  description: 'Water bill',
+  amount: 9800,
+  note: 'This shit is too expensive'
+}))
+
+store.dispatch(addExpense({
+  description: 'Gas bill',
+  amount: 5800,
+  note: 'This shit isn\'t too bad'
+}));
+
+store.dispatch(setTextFilter('water'));
+
+const {expenses, filters} = store.getState()
+
+const result = getVisibleExpenses(expenses, filters);
+
+console.log(result);
+
+// console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
